e74fd40d019a6bf127c48d27299137f09d233a04
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-01 12:27:22 +0800

    create-probot-app init

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = (robot) => {
+  // Your code here
+  console.log('Yay, the app was loaded!123')
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
