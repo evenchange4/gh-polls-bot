c5b3edb71e72bcdd21a519ed416d674daec74320
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-01 14:19:12 +0800

    now deploy

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 module.exports = (robot) => {
   // Your code here
-  console.log('Yay, the app was loaded!123')
+  console.log('Yay, the app was loaded!')
+  robot.on("issues.opened", async context => {
+    // An issue was just opened.
+    console.log("issues.opened");
+    robot.log(context);
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
---
+++ tasks/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -exu
+
+# ENV Variables
+DOT_ENV=".env"
+# NOW config
+TEAM='gh-polls-bot'
+PROJECT='gh-polls-bot'
+ALIAS='gh-polls-bot.now.sh'
+
+export PATH="./node_modules/.bin:$PATH"
+
+# 0. Switch to team
+now switch $TEAM
+# 1. Wair for deployment ready
+URL=$(now --dotenv="$DOT_ENV" --public)
+await-url "$URL/probot"
+now ls
+# # 2. Alias
+now alias set "$URL" "$ALIAS"
+# # 3. Purge old services
+now remove --yes --safe $PROJECT
+# # 4. Scale to 1
+now scale "$ALIAS" 1
+# # 5. Log results
+now ls
+now alias ls
