d2da5e74bc947b5d50c00c289a36d5e3ec9bd0a5
TJ Holowaychuk <tj@apex.sh>
2017-11-04 21:18:54 -0700

    fix BASE_URL to point to official api

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@
  * ref: https://github.com/srph/gh-polls-web/blob/master/src/config.js
  */
 const BASE_URL /* : string */ =
-  'https://m131jyck4m.execute-api.us-west-2.amazonaws.com/prod';
+  'https://api.gh-polls.com';
 const LABEL /* : string */ = 'Polls';
 
 module.exports = {
