33eec5bd0113f2d21c0509085c7b30c88674b958
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-11-07 11:38:07 +0800

    update

--- src/__tests__/listener.test.js
+++ src/__tests__/listener.test.js
@@ -101,36 +101,12 @@ it('should not performs actions without command matched issue#13', async () => {
     payload: {
       issue: {
         number: 1234,
-        body: `The tests have been failing for the past few days when trying to run bundle install. Here's the error:
-
-Errno::ENOENT: No such file or directory @ rb_sysopen -
-[![](https://api.gh-polls.com/poll/01BY90GJFJ0CFBJ19KBZY2Z43C/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable)](https://api.gh-polls.com/poll/01BY90GJFJ0CFBJ19KBZY2Z43C/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable/vote)
+        body: `Errno::ENOENT: No such file or directory @ rb_sysopen -
+/home/travis/.rvm/rubies/ruby-2.4.0/lib/ruby/site_ruby/2.4.0/bundler/templates/Executable
 An error occurred while installing concurrent-ruby (1.0.5), and Bundler cannot
 continue.
-Make sure that gem install concurrent-ruby - v '1.0.5' succeeds before
-bundling.
-In Gemfile:
-  github-pages was resolved to 167, which depends on
-    jekyll-mentions was resolved to 1.2.0, which depends on
-      html-pipeline was resolved to 2.7.1, which depends on
-        activesupport was resolved to 4.2.9, which depends on
-          i18n was resolved to 0.9.1, which depends on
-            concurrent-ruby
-Errno::ENOENT: No such file or directory @ rb_sysopen -
-[![](https://api.gh-polls.com/poll/01BY90GKADJF2DZPDZB6G96JN3/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable)](https://api.gh-polls.com/poll/01BY90GKADJF2DZPDZB6G96JN3/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable/vote)
-An error occurred while installing minitest (5.10.3), and Bundler cannot
-continue.
-Make sure that gem install minitest - v '5.10.3' succeeds before bundling.
-In Gemfile:
-  github-pages was resolved to 167, which depends on
-    jekyll-mentions was resolved to 1.2.0, which depends on
-      html-pipeline was resolved to 2.7.1, which depends on
-        activesupport was resolved to 4.2.9, which depends on
-          minitest
-Command exited with non-zero status 5
-20.82user 3.10system 0:26.66elapsed 89%CPU (0avgtext+0avgdata 205432maxresident)k
-0inputs+256704outputs (0major+867999minor)pagefaults 0swaps
-Example build: https://travis-ci.org/probot/probot.github.io/builds/298042595`,
+Make sure that gem install concurrent-ruby -v '1.0.5' succeeds before
+bundling.`,
         labels: [],
       },
     },
--- src/utils/__tests__/getCommand.test.js
+++ src/utils/__tests__/getCommand.test.js
@@ -1,14 +1,54 @@
 // @flow
 const getCommand = require('../getCommand');
 
+it('should return correct regular expression', () => {
+  expect(getCommand(`/polls`)).toEqual([]);
+  expect(getCommand(`/polls `)).toEqual(['/polls ', '']);
+  expect(getCommand(`/polls 1`)).toEqual(['/polls 1', '1']);
+  expect(getCommand(`/Polls 1`)).toEqual([]);
+  expect(getCommand(`/pollss 1`)).toEqual([]);
+  expect(getCommand(`/polls 1 2`)).toEqual(['/polls 1 2', '1 2']);
+  expect(getCommand(`/polls 1 2  3`)).toEqual(['/polls 1 2  3', '1 2  3']);
+  expect(getCommand(`/polls 1 2  3 4 `)).toEqual([
+    '/polls 1 2  3 4 ',
+    '1 2  3 4 ',
+  ]);
+  expect(
+    getCommand(`/polls 1 2  3 4
+5`),
+  ).toEqual(['/polls 1 2  3 4', '1 2  3 4']);
+  expect(getCommand(`/polls 1 '23' "5 5" 66`)).toEqual([
+    `/polls 1 '23' "5 5" 66`,
+    `1 '23' "5 5" 66`,
+  ]);
+  expect(getCommand(`/polls option1 'option 2' "option 3" `)).toEqual([
+    `/polls option1 'option 2' "option 3" `,
+    `option1 'option 2' "option 3" `,
+  ]);
+  expect(getCommand(`/polls/travis/.rvm/rubies`)).toEqual([]);
+  expect(getCommand(`/polls  1  2  3  `)).toEqual([
+    `/polls  1  2  3  `,
+    `1  2  3  `,
+  ]);
+  expect(getCommand(`/polls\t1\t2\t3\ttab`)).toEqual([
+    `/polls\t1\t2\t3\ttab`,
+    `1\t2\t3\ttab`,
+  ]);
+  expect(
+    getCommand(
+      `/home/travis/.rvm/rubies/ruby-2.4.0/lib/ruby/site_ruby/2.4.0/bundler/templates/Executable`,
+    ),
+  ).toEqual([]);
+});
+
 it('should return command with body content', () => {
   const body = `
 # H1
 ## H2
 > code
 /polls 1 2 3
-  `;
-  expect(getCommand(body)).toMatchSnapshot();
+  4`;
+  expect(getCommand(body)).toEqual(['/polls 1 2 3', '1 2 3']);
 });
 
 it('should return only one command', () => {
@@ -19,7 +59,7 @@ it('should return only one command', () => {
 /polls 4 5 6
 /polls 1 2 3
   `;
-  expect(getCommand(body)).toMatchSnapshot();
+  expect(getCommand(body)).toEqual(['/polls 4 5 6', '4 5 6']);
 });
 
 it('should return the first matched command', () => {
@@ -30,7 +70,7 @@ it('should return the first matched command', () => {
 > code
 /polls 1 2 3
   `;
-  expect(getCommand(body)).toMatchSnapshot();
+  expect(getCommand(body)).toEqual(['/polls 7 8 69', '7 8 69']);
 });
 
 it('should return command without argument', () => {
@@ -39,41 +79,15 @@ it('should return command without argument', () => {
 ## H2
 /polls
   `;
-  expect(getCommand(body)).toMatchSnapshot();
+  expect(getCommand(body)).toEqual([]);
 });
 
 it('should return empty array without any argument matched issue#13', () => {
-  const body = `
-The tests have been failing for the past few days when trying to run bundle install. Here's the error:
-
-Errno::ENOENT: No such file or directory @ rb_sysopen -
-[![](https://api.gh-polls.com/poll/01BY90GJFJ0CFBJ19KBZY2Z43C/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable)](https://api.gh-polls.com/poll/01BY90GJFJ0CFBJ19KBZY2Z43C/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable/vote)
+  const body = `Errno::ENOENT: No such file or directory @ rb_sysopen -
+/home/travis/.rvm/rubies/ruby-2.4.0/lib/ruby/site_ruby/2.4.0/bundler/templates/Executable
 An error occurred while installing concurrent-ruby (1.0.5), and Bundler cannot
 continue.
-Make sure that gem install concurrent-ruby - v '1.0.5' succeeds before
-bundling.
-In Gemfile:
-  github-pages was resolved to 167, which depends on
-    jekyll-mentions was resolved to 1.2.0, which depends on
-      html-pipeline was resolved to 2.7.1, which depends on
-        activesupport was resolved to 4.2.9, which depends on
-          i18n was resolved to 0.9.1, which depends on
-            concurrent-ruby
-Errno::ENOENT: No such file or directory @ rb_sysopen -
-[![](https://api.gh-polls.com/poll/01BY90GKADJF2DZPDZB6G96JN3/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable)](https://api.gh-polls.com/poll/01BY90GKADJF2DZPDZB6G96JN3/%2Ftravis%2F.rvm%2Frubies%2Fruby-2.4.0%2Flib%2Fruby%2Fsite_ruby%2F2.4.0%2Fbundler%2Ftemplates%2FExecutable/vote)
-An error occurred while installing minitest (5.10.3), and Bundler cannot
-continue.
-Make sure that gem install minitest - v '5.10.3' succeeds before bundling.
-In Gemfile:
-  github-pages was resolved to 167, which depends on
-    jekyll-mentions was resolved to 1.2.0, which depends on
-      html-pipeline was resolved to 2.7.1, which depends on
-        activesupport was resolved to 4.2.9, which depends on
-          minitest
-Command exited with non-zero status 5
-20.82user 3.10system 0:26.66elapsed 89%CPU (0avgtext+0avgdata 205432maxresident)k
-0inputs+256704outputs (0major+867999minor)pagefaults 0swaps
-Example build: https://travis-ci.org/probot/probot.github.io/builds/298042595
-  `;
-  expect(getCommand(body)).toMatchSnapshot();
+Make sure that gem install concurrent-ruby -v '1.0.5' succeeds before
+bundling.`;
+  expect(getCommand(body)).toEqual([]);
 });
